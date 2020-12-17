2f563ee8d1f9cecdb985b023e0eb5158d08ae202
gio <[omitted]>
2020-12-17 20:30:45 +0100

    Done adapting HistoricalRateLimiter

--- src/main/scala/com/gulo/ibclient/util/HistoricalRateLimiter.scala
+++ src/main/scala/com/gulo/ibclient/util/HistoricalRateLimiter.scala
@@ -8,14 +8,14 @@ import org.slf4j.{Logger, LoggerFactory}
 
 
 /*
- * Stateful utility to account for historical requests in order no to violate historical data limitations as specified in:
-
+ * Stateful utility to account for historical requests in order
+ * not to violate historical data limitations as specified in:
  * https://interactivebrokers.github.io/tws-api/historical_limitations.html
  * The basic usage is done through the following calls:
- * Requests should be accounted for with the "requested" method
- * nextRequestAfter_ms gives us the wait time until the next request can be made
- * cleanup will free the accounting of old requests, should be called periodically
- * */
+ *   Requests should be accounted for with the "requested" method
+ *   nextRequestAfter_ms gives us the wait time until the next request can be made
+ *   cleanup will free the accounting of old requests, should be called periodically
+ */
 
 class HistoricalRateLimiter {
   private[this] val log: Logger = LoggerFactory.getLogger(this.getClass)
@@ -32,21 +32,23 @@ class HistoricalRateLimiter {
 
   def now_ms: Long = Calendar.getInstance().getTimeInMillis()
 
-  // Put a single request into the `requests` TreeMultiMap
+  /* Put a single request into the `requests` TreeMultiMap.
+   * @reftime_ms can be used by the method below to assign a delay to this requests
+   * in case we're violating a TWS rule!
+   */
   def requested(
     request: HistoricalRequest,
     reftime_ms: Option[Long] = None
   ): Unit = {
     var curTime = reftime_ms.getOrElse(now_ms)
+    // apparently TreeMultiMap.put returns false if the key you're trying to add already exists...
     while (requests.put(curTime, request) == false) {
-      log.warn(s"Incrememtomg reftime of request ${request}, another request with the same time ${curTime}")
+      log.warn(s"Incrementing reftime of request ${request}, as there's already a request with the same time ${curTime}")
       curTime += 1
     }
   }
   
-  /*
-   * Take single HistoricalRequest from requests within a given timeframe
-   */
+  /* Take single HistoricalRequest from requests within a given timeframe */
   protected def latestInLast(
     timeframe_ms: Long,
     reftime_ms: Long = now_ms
@@ -56,10 +58,11 @@ class HistoricalRateLimiter {
     }
   }
 
-  /* 
-   * We know that the TWS API has some restrictions: you can send max N requests (@numRequests) of some type (@filter) in a given timeframe (@restrictionWait_ms).
-   * Of course there are restrictions that apply to all requests, that's why @filter is optional.
-   * So we retrieve all the latest requests in the relevant timeframe from @reftime_ms (normally = now), we apply the filter and then check they don't exceed @numRequests.
+  /* We know that the TWS API has some restrictions: you can send max N requests (@numRequests)
+   * of some type (@filter) in a given timeframe (@restrictionWait_ms).
+   * Of course there are also restrictions that apply to all requests, that's why @filter is optional.
+   * So we retrieve all the latest requests in the relevant timeframe from @reftime_ms (normally = now),
+   * we apply the filter and then check they don't exceed @numRequests.
    * If they do, we increase the waiting time. See L78
    * This func is used by nextRequestAfter_ms below.
    */
@@ -87,25 +90,47 @@ class HistoricalRateLimiter {
   }
 
   /* Given an HistoricalRequest and our `requests` "queue"
-   * Calculate the waiting time before we can place this request
+   * Calculate the waiting time before we can place this request in order to respect the restrictions
+   * TODO: discuss the `synchronized` call
    */
   def nextRequestAfter_ms(
     request: HistoricalRequest,
     reftime_ms: Long = now_ms
   ): Long = synchronized {
     var after_ms: Long = 0L
-    // Restriction number 1: there's a limit on the max num of identical req
+    
+    // Restriction number 1: no identical req in 15 seconds
     def identicalReq(req: HistoricalRequest): Boolean = { req == request }
     after_ms = math.max(after_ms, nextSlot_ms(15L * 1000, 1, Some(identicalReq), reftime_ms))
-    log.debug(s"delayed by ${after_ms} ms (due to limit on identical requests within 15 s)")
+    log.debug(s"delayed by ${after_ms}ms (due to limit on identical requests within 15 s)")
 
     // Restriction number 2: no more than 6 req for the same contract within 2 s
     def sameContract(req: HistoricalRequest) =  {
       (req.contract, req.exchange, req.barSize) == (request.contract, request.exchange, request.barSize)
     }
     after_ms = math.max(after_ms, nextSlot_ms(2L * 1000, 5, Some(sameContract), reftime_ms))
-    log.debug(s"delayed ${after_ms} ms (no more than 6 request for the same contract within 2s)")
+    log.debug(s"delayed by ${after_ms}ms (no more than 6 request for the same contract within 2s)")
 
+    // Restriction 3: no more than 60 requests in 10 minutes
+    after_ms = math.max(after_ms, nextSlot_ms(10L * 60 * 1000, 60, None, reftime_ms))
+    log.debug(s"delayed by ${after_ms}ms no more than 60 requests in 10 min")
+    
+    if (after_ms > 0) log.info(s"HistoricalRateLimiter: delaying request for ${after_ms/1000}s") 
     after_ms
+  }
+
+  def registerAndGetWait_ms(request: HistoricalRequest): Long = synchronized {
+    val wait_ms = nextRequestAfter_ms(request)
+    requested(request, Some(now_ms + wait_ms))
+    wait_ms
+  }
+
+  def cleanupAfter(time_ms: Long): Unit = synchronized {
+    val expired = requests.keys.asScala.filter(_ < time_ms)
+    expired.foreach(key => requests.removeAll(key))
+  }
+
+  def cleanup(reftime_ms: Long = now_ms): Unit = {
+    cleanupAfter(reftime_ms + 10L * 60 * 1000)
   }
 }
